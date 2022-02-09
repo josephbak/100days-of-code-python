d42a66c0d74dabbc6acbe4d3336e95af5e122c44
Joseph Bak <joseph.bak31415@gmail.com>
2022-02-09 12:01:09 -0500

    day 32 project

---
+++ day32-SMTP-Cloud-email/README.md
@@ -0,0 +1,3 @@
+# Smtplib practice
+This program sends a motivational quote at a specific day by an email.
+This is used for practicing how to work with smtplib module and files. 
---
+++ day32-SMTP-Cloud-email/main.py
@@ -0,0 +1,45 @@
+import smtplib
+import datetime as dt
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.getenv("MY_PASSWORD")
+
+#
+# with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+#     connection.starttls()
+#     connection.login(user= my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="programming.31415@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+# print(day_of_week)
+#
+#
+# date_of_birth = dt.datetime(year= 2001, month= 1, day=1, hour= 1)
+# print(date_of_birth)
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0: # 0 is Monday
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=MY_EMAIL,
+                            msg=f"Subject: Monday Motivation\n\n{quote}"
+                            )
